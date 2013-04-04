3b10b9cd4db790d474e7b423de40c193645137dc
Abhishek Gopalan <abhi4717@gmail.com>
2013-04-04 22:38:21 +0530

    Initial project setup

---
+++ MsBuild/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MsBuild
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
