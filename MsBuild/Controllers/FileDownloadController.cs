6d126417c53b0d20d8d1e65eef093ca19c3ecb93
Abhishek Gopalan <abhi4717@gmail.com>
2013-06-06 13:25:50 +0530

    Added test controller to check file download

---
+++ MsBuild/Controllers/FileDownloadController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace MsBuild.Controllers
+{
+    public class FileDownloadController : Controller
+    {
+        //
+        // GET: /FileDownload/
+
+        public ActionResult Index()
+        {
+            return new FilePathResult("~/App_Data/Test.xlsx", "application/txt") { FileDownloadName = "Test.xlsx" };
+        }
+
+    }
+}
